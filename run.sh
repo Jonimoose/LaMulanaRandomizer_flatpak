73d7c44fb6971bf39d7d384fdaee2d7b394a5044
Jon Sturm <jonimoose@gmail.com>
2024-08-15 21:19:27 -0500

    Update Icon along with additional adjustments.

--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+if [[ -z "${XDG_CONFIG_HOME}" ]]; then
+  RANDO_DIRECTORY="~/.config/LaMulana_Ramdomizer"
+else
+  RANDO_DIRECTORY="${XDG_CONFIG_HOME}/LaMulana_Randomizer"
+fi
+
+mkdir -p $RANDO_DIRECTORY
+cd $RANDO_DIRECTORY
+if [[ ! -f "randomizer-config.txt" ]]; then
+  echo "laMulanaBaseDir=/home/deck/.local/share/Steam/steamapps/common/La-Mulana" >> randomizer-config.txt
+  echo "laMulanaSaveDir=/home/deck/.steam/steam/steamapps/compatdata/230700/pfx/drive_c/users/steamuser/Documents/nigoro/la-mulana/save" >> randomizer-config.txt
+fi
 java -jar /app/bin/randomizer-2.36.0.jar
